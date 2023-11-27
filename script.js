8cf15ff8de7b1c1e3eb9cecaaa71418bb9ed8b33
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-27 17:33:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 0) {
+        return "Invalid input. Please provide a positive integer.";
+    }
+
+    if (num === 1) {
+        return 0;
+    }
+
+    let first = 0;
+    let second = 1;
+
+    for (let i = 2; i < num; i++) {
+        let next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return second;
 }
 
 module.exports = fibonacci;
